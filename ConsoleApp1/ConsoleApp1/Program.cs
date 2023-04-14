b0b610c25d1b7e56831ef82c48f84b9a5b262109
André <AndreR@ppedv.de>
2023-04-14 10:55:59 +0200

    Rest Client

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+swaggerClient client = new swaggerClient("https://localhost:7219/",new HttpClient());
+
+var json = client.GetWeatherForecastAsync().Result;
+Console.WriteLine(json);
