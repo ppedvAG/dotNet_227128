e214f350a3a7ceee41d2fd155c37f02a2233a785
André <AndreR@ppedv.de>
2023-04-14 10:56:12 +0200

    GRPC

---
+++ ConsoleAppGRPC_Client/ConsoleAppGRPC_Client/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcService1;
+
+Console.WriteLine("Hello, World!");
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7106");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient HALLO" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey(); 
