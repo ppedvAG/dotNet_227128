c067b5f8fbb917470424e3f7622fa7349c871356
André <AndreR@ppedv.de>
2023-04-14 15:06:19 +0200

    Async/Await

--- HalloAsync/HalloAsync/Form1.Designer.cs
+++ HalloAsync/HalloAsync/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             button1 = new Button();
             progressBar1 = new ProgressBar();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -49,12 +53,56 @@
             progressBar1.Size = new Size(596, 46);
             progressBar1.TabIndex = 1;
             // 
+            // button2
+            // 
+            button2.Location = new Point(147, 218);
+            button2.Name = "button2";
+            button2.Size = new Size(417, 46);
+            button2.TabIndex = 0;
+            button2.Text = "Methodinvoke";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(147, 296);
+            button3.Name = "button3";
+            button3.Size = new Size(417, 46);
+            button3.TabIndex = 2;
+            button3.Text = "Mit TS";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(62, 376);
+            button4.Name = "button4";
+            button4.Size = new Size(417, 46);
+            button4.TabIndex = 3;
+            button4.Text = "Mit Async/Await";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(147, 428);
+            button5.Name = "button5";
+            button5.Size = new Size(417, 46);
+            button5.TabIndex = 4;
+            button5.Text = "MyOld Func";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 564);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(progressBar1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -65,5 +113,9 @@
 
         private Button button1;
         private ProgressBar progressBar1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
--- HalloAsync/HalloAsync/Form1.cs
+++ HalloAsync/HalloAsync/Form1.cs
@@ -9,15 +9,71 @@ namespace HalloAsync
 
         private void button1_Click(object sender, EventArgs e)
         {
+            for (int i = 0; i < 100; i++)
+            {
+                progressBar1.Value = i;
+                Thread.Sleep(300);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
             Task.Run(() =>
             {
                 for (int i = 0; i < 100; i++)
                 {
-                    progressBar1.Value = i;
+                    //progressBar1.Value = i;
+                    progressBar1.Invoke(new Action(() => { progressBar1.Value = i; }));
                     Thread.Sleep(300);
                 }
             });
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var ts = TaskScheduler.FromCurrentSynchronizationContext();
+            button3.Enabled = false;
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Task.Factory.StartNew(() => progressBar1.Value = i, CancellationToken.None, TaskCreationOptions.None, ts);
+                    Thread.Sleep(30);
+                }
+            }).ContinueWith(t => button3.Enabled = true, CancellationToken.None, TaskContinuationOptions.None, ts);
+
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+
+            for (int i = 0; i < 100; i++)
+            {
+                progressBar1.Value = i;
+
+                await Task.Delay(30);
+            }
+
+            button3.Enabled = true;
+        }
+
+        private long AlteLangesameFunc(int zahl)
+        {
+            Thread.Sleep(5000);
+            return 923847 * zahl;
+        }
+
+        
+        private ValueTask<long> AlteLangesameFuncAsync(int zahl) 
+        {
+            return Task.Run(() => AlteLangesameFunc(zahl));
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show($"OLD Func: {AlteLangesameFunc(16)}");
+            MessageBox.Show($"OLD Func: {await AlteLangesameFuncAsync(16)}");
         }
     }
 }
